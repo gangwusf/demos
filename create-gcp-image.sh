d1b647ea71896f8bf3d5eeb9b704b8a95e1370fc
Greg Wu <gregwu@yahoo.com>
2023-02-27 10:09:54 -0800

    push

--- create-gcp-image.sh
+++ create-gcp-image.sh
@@ -1,7 +1,12 @@
 
+TAG="gcr.io//warm-tome-332217/demos"
+gcloud builds submit . -t=$TAG 
+
+
 TAG="demos"
 docker build -t $TAG .
 docker tag $TAG gcr.io/warm-tome-332217/$TAG:v1
 docker push gcr.io/warm-tome-332217/$TAG:v1
+
 #kubectl delete -f deployment.yml
 kubectl apply -f  deployment.yml
